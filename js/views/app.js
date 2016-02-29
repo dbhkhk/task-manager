1c896efd8c00edda20943df6764cbbb236c8afa1
Brody Chen <rufengch@gmail.com>
2016-02-28 20:38:35 -0800

    feat: Check due date input and format as MM/DD

--- js/views/app.js
+++ js/views/app.js
@@ -51,11 +51,24 @@ app.AppView = Backbone.View.extend({
 
 	// get attributes from input and generate an obj for createTask
 	newAttributes: function(){
-		return {
+		var due = this.$due.val().trim();
+		var length = due.length;
+		var result = {
 			name: this.$name.val().trim(),
-			note: this.$note.val().trim(),
-			due: this.$due.val().trim()
+			note: this.$note.val().trim()
 		};
+		// check the format of date input and make it standard as MM/DD
+		if (length === 3) {
+			due = '0' + due.slice(0, 2) + '0' + due.slice(2);
+		} else if (length === 4) {
+			if (due.indexOf('/') === 1) {
+				due = '0' + due;
+			} else {
+				due = due.slice(0,3) + '0' + due.slice(3);
+			}
+		}
+		result.due = due;
+		return result;
 	},
 
 	clearInput: function(){
