6487c96e178bcdb91048c76caa973b92277d71de
Brody Chen <rufengch@gmail.com>
2016-02-28 19:43:50 -0800

    docs: Add comments for TaskView

--- js/views/task.js
+++ js/views/task.js
@@ -4,13 +4,15 @@ var app = app || {};
 
 // TaskView is view for a single model
 app.TaskView = Backbone.View.extend({
-	tagName: 'td',
+	tagName: 'td', // create a <td> element for each model to render in the table
 
 	events: {
 		'click .destroy': 'clear'
 	},
 
 	initialize: function(){
+		// 'model' attribute will be passed in when a TodoView is created
+		// remove the view and its el from the DOM, also stop all listening
 		this.listenTo(this.model, 'destroy', this.remove);
 	},
 
@@ -19,6 +21,7 @@ app.TaskView = Backbone.View.extend({
 		return this;
 	},
 
+	// destroy the model on server
 	clear: function(){
 		this.model.destroy();
 	}
